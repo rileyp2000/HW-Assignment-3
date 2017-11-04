1757e88766de0078ee94e2a0495a2f453454d34a
rileyp2000 <rileyp@bishopireton.org>
2017-11-04 17:49:14 -0400

    added fish and output files from indexmakerv1

--- IndexMakerV2/src/IndexEntry.java
+++ IndexMakerV2/src/IndexEntry.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 /**
  * <p></p>
@@ -9,6 +10,7 @@ public class IndexEntry {
 	
 	private String word;
 	private ArrayList<Integer> numsList;
+	private TreeSet<Integer> lineNums;
 	
 	/**
 	 * <p>Constructs a new IndexEntry object</p>
@@ -16,7 +18,8 @@ public class IndexEntry {
 	 */
 	public IndexEntry(String wd){
 		word = wd.toUpperCase();
-		numsList = new ArrayList<Integer>();
+		//numsList = new ArrayList<Integer>();
+		lineNums = new TreeSet<Integer>();
 	}
 	
 	/**
@@ -28,13 +31,15 @@ public class IndexEntry {
 	}
 	
 	public void add(int num){
-		if(!numsList.contains(num))
-			numsList.add(num);
+		/*if(!numsList.contains(num))
+			numsList.add(num);*/
+		if(!lineNums.contains(num))
+			lineNums.add(num);
 	}
 
 	@Override
 	public String toString() {
-		return "IndexEntry [word=" + word + ", numsList=" + numsList + "]";
+		return "IndexEntry for " + word + "Appears on lines: " + lineNums;
 	}
 	
 	
